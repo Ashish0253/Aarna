50880093a21887e14d670b75632c320557c98b74
Ashish0253 <ashishrana0253@gmail.com>
2022-12-22 10:11:52 +0530

    hover effect added to navbar

--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -10,26 +10,29 @@ export default function Navbar() {
         className="w-[202px] inline mx-[100px] my-[30px] cursor-pointer"
         href="localhost:3000"
       />
-      <a href="/" className="ml-[100px] mr-4 text-lg font-semibold">
+      <a
+        href="/"
+        className="ml-[100px] mr-4 text-lg font-semibold hover:text-gray-500"
+      >
         Home{" "}
       </a>
-      <a href="/" className="m-4 text-lg font-semibold">
+      <a href="/" className="m-4 text-lg font-semibold hover:text-gray-500">
         Aboutus
       </a>
-      <a href="/" className="m-4 text-lg font-semibold">
+      <a href="/" className="m-4 text-lg font-semibold hover:text-gray-500">
         Department
       </a>
-      <a href="/" className="m-4 text-lg font-semibold">
+      <a href="/" className="m-4 text-lg font-semibold hover:text-gray-500">
         Services
       </a>
-      <a href="/" className="m-4 text-lg font-semibold">
+      <a href="/" className="m-4 text-lg font-semibold hover:text-gray-500">
         Doctors
       </a>
-      <a href="/" className="m-4 text-lg font-semibold">
+      <a href="/" className="m-4 text-lg font-semibold hover:text-gray-500">
         Contactus
       </a>
 
-      <button class="rounded border-white border-2 ml-[200px] mt-[15px] text-white bg-[#e91e63] p-2">
+      <button class="rounded border-white border-2 ml-[200px] mt-[15px] text-white bg-[#e91e63] p-2 hover:bg-gray-500">
         Appointment
       </button>
     </div>
