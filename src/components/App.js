9f9677184ff2408b4412cc9c949ab2d8dd62fdcd
vishu <bansalvishu56@gmail.com>
2022-12-25 18:32:46 +0530

    Carousel added

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,13 @@ import Departments from "./department/Departments";
 // import Stickyfooter from "./footer/Stickyfooter";
 import Map from "./map/Map";
 // import BodyParts from "./bodyParts/BodyParts";
+import Carousel from "./carousel/Carousel";
 
 function App() {
   return (
     <div>
       <Header />
+      <Carousel/>
       <Departments />
       {/* <BodyParts /> */}
       <Map />
--- src/components/carousel/Testimonials.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Carousel from "./Carousel";
-
-function Testimonials(){
-    return <Carousel/>
-}
-
-export default Testimonials;
