907e50063f0e917bf3ae37b3bf9fa5c27bbdeb16
vishu <bansalvishu56@gmail.com>
2022-12-26 19:05:58 +0530

    Mapping functionality added in crousel

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -2,13 +2,13 @@ import React from "react";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import C1 from "./C-Images/C1.png"
-import C2 from "./C-Images/C2.png"
-import C3 from "./C-Images/C3.jpg"
-import C4 from "./C-Images/C4.jpg"
-import C5 from "./C-Images/C5.jpg"
+import images from "./Images";
 
-
+/* 
+buttons in carousel
+make new carousel for ultrasound,etc
+do work on styling mainly of testimonial section
+*/
 function Carousel(){
 
     const settings = {
@@ -23,28 +23,23 @@ function Carousel(){
         speed: 1500,
         autoplaySpeed: 5000,
         cssEase: "linear",
-        pauseOnHover: true,
+        pauseOnHover: false,
       };
 
     return (
-        <div className="py-10 px-8 w-full h-full">
+        <div className="w-full h-full">
           <Slider {...settings}>
-            <div>
-              <img src={C1} alt="images" className="rounded-md h-[600px] w-full border-8 border-[#1a0e69]"/>
-            </div>
-            <div>
-              <img src={C2} alt="images" className="rounded-md h-[600px] w-full border-8 border-[#1a0e69]"/>
-            </div>
-            <div>
-            <img src={C3} alt="images" className="rounded-md h-[600px] w-full border-8 border-[#1a0e69]"/>
-            </div>
-            <div>
-            <img src={C4} alt="images" className="rounded-md h-[600px] w-full border-8 border-[#1a0e69]"/>
-            </div>
-            <div>
-            <img src={C5} alt="images" className="rounded-md h-[600px] w-full border-8 border-[#1a0e69]"/>
-            </div>
-            
+          {
+            images.map(item => {
+              return (
+                <div>
+              <img src={item.source} alt="images" className="rounded-md h-[600px] w-full"/>
+            </div>   
+              )
+            }
+            )
+          }
+                   
           </Slider>
           <hr class="my-8 h-px bg-gray-200 border-0 dark:bg-gray-700"/>
 
