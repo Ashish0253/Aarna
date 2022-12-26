03047649d8eb62615668d3ce304e9fde26a29604
Ashish0253 <ashishrana0253@gmail.com>
2022-12-26 23:16:21 +0530

    removed merge problem again

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -5,14 +5,7 @@ import Slider from "react-slick";
 
 import images from "./Images";
 
-import C1 from "./C-Images/C1.png";
-import C2 from "./C-Images/C2.png";
-import C3 from "./C-Images/C3.jpg";
-import C4 from "./C-Images/C4.jpg";
-import C5 from "./C-Images/C5.jpg";
-
-
-function Carousel() {
+export default function Carousel() {
   const settings = {
     dots: false,
     infinite: true,
@@ -23,90 +16,26 @@ function Carousel() {
     fade: true,
     // eslint-disable-next-line no-dupe-keys
     speed: 1500,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 5000,
     cssEase: "linear",
     pauseOnHover: false,
   };
 
-
-function Carousel(){
-
-    const settings = {
-        dots: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        fade: true,
-        // eslint-disable-next-line no-dupe-keys
-        speed: 1500,
-        autoplaySpeed: 5000,
-        cssEase: "linear",
-        pauseOnHover: false,
-      };
-
-    return (
-        <div className="w-full h-full px-7 mb-3">
-          <Slider {...settings}>
-          {
-            images.map(item => {
-              return (
-                <div>
-              <img src={item.source} alt="images" className="rounded-md h-[600px] w-full"/>
-            </div>   
-              )
-            }
-            )
-          }
-                   
-          </Slider>
-          {/* <hr class="my-8 h-px bg-gray-200 border-0 dark:bg-gray-700"/> */}
-
-
   return (
-    <div className="bg-primary py-10 px-8 w-full h-full">
+    <div className="w-full h-full px-7 mb-3">
       <Slider {...settings}>
-        <div>
-          <img
-            src={C1}
-            alt="images"
-            className="rounded-md h-[600px] w-full border-8 border-[#1a0e69]"
-          />
-        </div>
-        <div>
-          <img
-            src={C2}
-            alt="images"
-            className="rounded-md h-[600px] w-full border-8 border-[#1a0e69]"
-          />
-        </div>
-        <div>
-          <img
-            src={C3}
-            alt="images"
-            className="rounded-md h-[600px] w-full border-8 border-[#1a0e69]"
-          />
-        </div>
-        <div>
-          <img
-            src={C4}
-            alt="images"
-            className="rounded-md h-[600px] w-full border-8 border-[#1a0e69]"
-          />
-        </div>
-        <div>
-          <img
-            src={C5}
-            alt="images"
-            className="rounded-md h-[600px] w-full border-8 border-[#1a0e69]"
-          />
-
-        </div>
+        {images.map((item) => {
+          return (
+            <div>
+              <img
+                src={item.source}
+                alt="images"
+                className="rounded-md h-[600px] w-full"
+              />
+            </div>
+          );
+        })}
       </Slider>
-      <hr class="my-8 h-px bg-gray-200 border-0 dark:bg-gray-700" />
     </div>
   );
 }
-
-export default Carousel;
