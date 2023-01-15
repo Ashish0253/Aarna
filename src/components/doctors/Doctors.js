b957e9bf7be25806f4eb3f8fc0051fd6cfd7bf38
vishu <bansalvishu56@gmail.com>
2023-01-15 15:59:12 +0530

    Doctors card changed

--- src/components/doctors/Doctors.js
+++ src/components/doctors/Doctors.js
@@ -4,53 +4,51 @@ import content from "./doc_data";
 
 export default function Doctors() {
   return (
-    <div className="pt-20 px-10">
+    <div className="pt-20 px-32">
       <h1 className="text-center mb-20 text-5xl font-bold text-black">
         Our Doctors
       </h1>
 
-      {content.map((item) => {
-        return (
-          <div className="inline-block">
-            <div className="p-1 pb-4">
-              <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-                <a href="#">
-                  <img class="rounded-t-lg" src={item.src} alt="" />
+      <div className="">
+        {content.map((item) => {
+          return (
+            <div className="inline-block rounded-xl overflow-hidden mx-1 mb-4 bg-slate-900">
+              <div>
+                <a href="">
+                  <img src={item.src} alt={item.id} className="h-96 w-80" />
                 </a>
-                <div class="p-5">
-                  <a href="#">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                      {item.name}
-                    </h5>
-                  </a>
-                  <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                    {item.desc}
-                  </p>
-                  <a
-                    href={item.linkedin}
-                    class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              </div>
+              <div className="p-3">
+                <p className="py-2 text-center text-[#f8f8f8] text-xl font-bold">
+                  {item.name}
+                </p>
+                <p className="pb-3 text-center text-[#f8f8f8] font-medium">
+                  {item.desc}
+                </p>
+                <a
+                  href={item.linkedin}
+                  class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  LinkedIn
+                  <svg
+                    aria-hidden="true"
+                    class="w-4 h-4 ml-2 -mr-1"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
                   >
-                    LinkedIn
-                    <svg
-                      aria-hidden="true"
-                      class="w-4 h-4 ml-2 -mr-1"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                        clip-rule="evenodd"
-                      ></path>
-                    </svg>
-                  </a>
-                </div>
+                    <path
+                      fill-rule="evenodd"
+                      d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                </a>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 }
