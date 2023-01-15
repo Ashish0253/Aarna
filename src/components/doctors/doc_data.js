1a8db515962e2a075fecbc73408ac6f58ba19b37
vishu <bansalvishu56@gmail.com>
2023-01-15 15:03:00 +0530

    Dummy Doc Page ready

---
+++ src/components/doctors/doc_data.js
@@ -0,0 +1,39 @@
+const content = [
+  {
+    id: 1,
+    src: "http://aarnadiagnosticcentre.com/wp-content/uploads/2021/05/Anil-1.png",
+    name: "Dr Anil Rajput",
+    desc: "MD Pathology",
+    linkedin: "https://www.linkedin.com/",
+  },
+  {
+    id: 1,
+    src: "http://aarnadiagnosticcentre.com/wp-content/uploads/2021/05/Anil-1.png",
+    name: "Dr Anil Rajput",
+    desc: "MD Pathology",
+    linkedin: "https://www.linkedin.com/",
+  },
+  {
+    id: 1,
+    src: "http://aarnadiagnosticcentre.com/wp-content/uploads/2021/05/Anil-1.png",
+    name: "Dr Anil Rajput",
+    desc: "MD Pathology",
+    linkedin: "https://www.linkedin.com/",
+  },
+  {
+    id: 1,
+    src: "http://aarnadiagnosticcentre.com/wp-content/uploads/2021/05/Anil-1.png",
+    name: "Dr Anil Rajput",
+    desc: "MD Pathology",
+    linkedin: "https://www.linkedin.com/",
+  },
+  {
+    id: 1,
+    src: "http://aarnadiagnosticcentre.com/wp-content/uploads/2021/05/Anil-1.png",
+    name: "Dr Anil Rajput",
+    desc: "MD Pathology",
+    linkedin: "https://www.linkedin.com/",
+  },
+];
+
+export default content;
